c69a3ebb43486f7db95795bd7c04ef6764436e5e
Caio Magalhães <89360553+CaioMagalhaes95@users.noreply.github.com>
2022-11-25 23:13:59 -0300

    back

--- client/LINCEHub-main/kora/web/js/funcoes.js
+++ client/LINCEHub-main/kora/web/js/funcoes.js
@@ -1,5 +1,13 @@
 
- function sRegister() {
-    alert('Cadastro Realizado com sucesso!');
-   
+
+
+const onHandleClick = (event) => {
+    event.preventDefault();
+    axios.post('http://localhost:3000/sub', {
+        nome: $('#name').val(),
+        RA: $('#RA').val(),
+        course: $('#curso').val()
+    });
 }
+
+   
--- index.js
+++ index.js
@@ -14,11 +14,20 @@ app.set('view engine', 'html')
 app.set('views', path.join(__dirname, './views'))
 
 
-mongoose.connect('mongodb://localhost:27017')//('mongodb+srv://Caio:mongodb@cluster1.ajekz50.mongodb.net/?retryWrites=true&w=majority')
-.then(() =>{
-    console.log("Conectado!");
-    app.listen(port, ()=>{
+// mongoose.connect('mongodb://localhost:27017')//'mongodb+srv://Caio:mongodb@cluster1.ajekz50.mongodb.net/?retryWrites=true&w=majority')//
+// .then(() =>{
+//     console.log("Conectado!");
+//     app.listen(port, ()=>{
         
-    })
-})
-.catch((err) => console.log(err))
+//     })
+// })
+// .catch((err) => console.log(err))
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://Caio:<Salvar17182910@>@cluster0.wljzu.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
