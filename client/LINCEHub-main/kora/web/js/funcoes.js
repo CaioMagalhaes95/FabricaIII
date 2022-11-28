4b7cf55323b0e1e0172447e0f673d308e86e5e81
Caio Magalhães <89360553+CaioMagalhaes95@users.noreply.github.com>
2022-11-28 10:37:15 -0300

    Backend is alive!

--- client/LINCEHub-main/kora/web/js/funcoes.js
+++ client/LINCEHub-main/kora/web/js/funcoes.js
@@ -1,13 +1,37 @@
 
-
-
 const onHandleClick = (event) => {
     event.preventDefault();
     axios.post('http://localhost:3000/sub', {
-        nome: $('#name').val(),
+        name: $('#name').val(),
         RA: $('#RA').val(),
-        course: $('#curso').val()
-    });
+        course: $('#course').val()
+    }).then(() => {
+        alert('Usuário Cadastrado com Sucesso')
+    }).catch(()=>{
+        alert('Campo inválido')
+    })
+
+    
+
+    $('#name').val('');
+    $('#RA').val('');
+    $('#course').val('');
+
 }
 
+const onHandleClickS = (event) => {
+    event.preventDefault();
+    axios.post('http://localhost:3000/sponsor',{
+        Razao_social: $('#rs').val(),
+        cnpj: $('#cnpj').val(),
+        email: $('#email').val(),
+        telefone: $('#tel').val(),
+        
+       
+    }).then(() => {
+        alert('Patrocinador Cadastrado com Sucesso')
+    }).catch(()=>{
+        alert('Campo inválido')
+    })
+}
    
--- index.js
+++ index.js
@@ -1,33 +1,35 @@
 const express = require('express');
 const mongoose = require('mongoose')
-
+const cors = require('cors')
 const app = express();
 const port = 3000;
 var path = require('path')
 
 const router = require('./server/routes/personRoutes.js')
 
+app.use(cors())
 app.use(router)
+
 app.use(express.json());
 app.engine('html', require('ejs').renderFile)
 app.set('view engine', 'html')
 app.set('views', path.join(__dirname, './views'))
 
 
-// mongoose.connect('mongodb://localhost:27017')//'mongodb+srv://Caio:mongodb@cluster1.ajekz50.mongodb.net/?retryWrites=true&w=majority')//
-// .then(() =>{
-//     console.log("Conectado!");
-//     app.listen(port, ()=>{
+mongoose.connect('mongodb://localhost:27017')//'mongodb+srv://Caio:mongodb@cluster1.ajekz50.mongodb.net/?retryWrites=true&w=majority')//
+.then(() =>{
+    console.log("Conectado!");
+    app.listen(port, ()=>{
         
-//     })
-// })
-// .catch((err) => console.log(err))
+    })
+})
+.catch((err) => console.log(err))
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://Caio:<Salvar17182910@>@cluster0.wljzu.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = "mongodb+srv://Caio:<Salvar17182910@>@cluster0.wljzu.mongodb.net/?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
